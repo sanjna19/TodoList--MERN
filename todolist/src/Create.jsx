2261e6da1680da0356660214fd7184d43db9eb4c
sanjna19 <schipp3@uic.edu>
2025-06-11 16:23:47 -0500

    css changes for input component

--- todolist/src/Create.jsx
+++ todolist/src/Create.jsx
@@ -4,7 +4,7 @@ function Create() {
   return (
     <div className='form'>
         
-        <input type="text" name="" id="" placeholder='Enter a Task'/>
+        <input class='input' type="text" name="" id="" placeholder='Enter a Task'/>
         <button class="button-54" role="button" type='button'>Add New Task</button>
         
     </div>
