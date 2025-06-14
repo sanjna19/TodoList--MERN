95b4b3cfe86a8cf2e8b6fa8cf1e79fa514253c43
sanjna19 <schipp3@uic.edu>
2025-06-14 16:35:50 -0500

    cors issue

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,16 @@ const corsOptions = {
   allowedHeaders: ["Content-Type"],
   credentials: true
 };
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('*', cors());
+
+app.use((req, res, next) => {
+  res.setheader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers", 
+    "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json())
 
